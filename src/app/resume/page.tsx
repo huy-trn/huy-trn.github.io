8c41c6a87ea8c8fd96f9e2d2c17fe3941922e6ec
tranquochuy645@gmail.com <tranquochuy645@gmail.com>
2025-03-25 14:06:27 +0700

    Fix PDF display

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,14 +1,61 @@
+'use client'
+
+import { useState, useCallback } from 'react';
+import { pdfjs, Document, Page } from 'react-pdf';
+import useResizeObserver from '../react-hooks/useResizeObserver';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+
+const CV_URL = "/Developer_HuyTran.pdf"; // Public folder reference
+const PDF_OPTIONS = {
+    cMapUrl: '/cmaps/',
+    standardFontDataUrl: '/standard_fonts/',
+};
+
 const Resume = () => {
-    const cvUrl = "/Developer_HuyTran.pdf"; // Public folder reference
+    const [numPages, setNumPages] = useState<number>();
+    const [containerRef, setContainerRef] = useState<HTMLElement | null>(null);
+    const [containerWidth, setContainerWidth] = useState<number>();
+
+    const onResize = useCallback<ResizeObserverCallback>((entries) => {
+        const [entry] = entries;
+
+        if (entry) {
+            setContainerWidth(entry.contentRect.width);
+        }
+    }, []);
+
+    useResizeObserver(containerRef, {}, onResize);
+
+    const onDocumentLoadSuccess = ({ numPages }: { numPages: number }): void => {
+        setNumPages(numPages);
+    }
 
     return (
-        <section className="flex grow w-full h-full p-4">
+        <section className="flex flex-col grow w-full h-full p-4" ref={setContainerRef} >
             {/* Display PDF using an embed element */}
-            <embed 
-                    src={cvUrl} 
-                    type="application/pdf" 
-                    className="grow bg-white overflow-scroll"
-                />
+            <Document file={CV_URL}
+                options={PDF_OPTIONS}
+                onLoadSuccess={onDocumentLoadSuccess}>
+                {Array.from(new Array(numPages), (_el, index) => (
+                    <Page
+                        key={`page_${index + 1}`}
+                        pageNumber={index + 1}
+                        width={containerWidth}
+                        className="mb-2"
+                    />
+                ))}
+            </Document>
+             {/* Download Link */}
+             <a
+                href={CV_URL}
+                download="HuyTran_Resume.pdf"
+                className="max-w-40 self-center mt-4 px-4 py-2 rounded-md transition border border-gray-300 text-gray-300 hover:border-blue-600 hover:text-blue-400"
+            >
+                Download CV
+            </a>
         </section>
     );
 };
