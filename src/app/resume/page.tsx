7c872ade5b9152e74d96cb13a1a81a6dd08a2085
tranquochuy645@gmail.com <tranquochuy645@gmail.com>
2025-03-25 13:07:45 +0700

    Fix CV display

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -2,12 +2,12 @@ const Resume = () => {
     const cvUrl = "/Developer_HuyTran.pdf"; // Public folder reference
 
     return (
-        <section className="flex grow w-full h-full md:p-0 p-4 bg-white">
+        <section className="flex grow w-full h-full p-4">
             {/* Display PDF using an embed element */}
             <embed 
                     src={cvUrl} 
                     type="application/pdf" 
-                    className="w-full grow"
+                    className="grow bg-white overflow-scroll"
                 />
         </section>
     );
